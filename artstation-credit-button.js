9d3b4535da7825acd150e735e61c57f189ad99cd
Dave <doctegl@gmail.com>
2021-11-09 14:44:19 +0100

    artstation credit button v1.0

--- artstation-credit-button.js
+++ artstation-credit-button.js
@@ -1,13 +1,56 @@
 // ==UserScript==
 // @name         Artstation quick-credit creator
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  Adds a button to quickly copy to clipboard the artwork's title and author
 // @author       Davide Stasio
 // @match        https://www.artstation.com/artwork/*
 // @icon         https://www.google.com/s2/favicons?domain=artstation.com
 // @grant        none
 // ==/UserScript==
+
+function DeleteToast(container, toast) {
+    var opacity = 1; // initial opacity
+    var timer = setInterval(function () {
+        if (opacity <= 0.1){
+            clearInterval(timer);
+            container.removeChild(toast);
+        }
+        toast.style.opacity = opacity;
+        toast.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
+        opacity -= opacity * 0.1;
+    }, 50);
+}
+
+function SpawnToast() {
+    var toast_container = document.getElementById("toast-container");
+    if (toast_container == null)
+    {
+        toast_container = document.createElement("div");
+        toast_container.setAttribute("id", "toast-container");
+        toast_container.className = "toast-top-left";
+
+        document.body.appendChild(toast_container);
+    }
+
+
+    var message = "Credit string copied to clipboard";
+    var toast = document.createElement("div");
+    toast.className = "toast";
+    toast.classList.add("toast-info");
+    toast.setAttribute("aria-live", "polite");
+    toast.style.display = "block";
+
+    toast.innerHTML = 
+    `<button type="button" class="toast-close-button" role="button">
+    <i class="far fa-times"></i>
+    </button>
+    <div class="toast-title">Notice</div>
+    <div class="toast-message">${message}</div>`;
+
+    toast_container.insertBefore(toast, toast_container.firstElementChild);
+    setTimeout(DeleteToast, 700, toast_container, toast);
+}
 
 function CopyCredit() {
     var credit_text = ""
@@ -17,7 +60,7 @@ function CopyCredit() {
     credit_text = title + " by " + author
 
     navigator.clipboard.writeText(credit_text);
-    alert("Credit copied to clipboard");
+    SpawnToast();
 }
 
 (function() {
@@ -32,8 +75,6 @@ function CopyCredit() {
     credit_button.appendChild(document.createTextNode("Copy credit"));
     credit_button.removeAttribute("ng-authorized-click");
     credit_button.addEventListener("click", CopyCredit);
-
-    console.log(credit_button);
 
     button_col.appendChild(credit_button);
 })();
